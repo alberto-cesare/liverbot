809d14d65b34f6a763eee32957a298d0da583bfb
unknown <barbonalberto@gmail.com>
2019-12-13 20:03:38 +0100

    R

--- execute.php
+++ execute.php
@@ -1,10 +1,10 @@
 <?php
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
-//if(!$update)
-//{
-//  exit;
-//}
+if(!$update)
+{
+  exit;
+}
 $message = isset($update['message']) ? $update['message'] : "";
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
@@ -17,47 +17,22 @@ $text = trim($text);
 $text = strtolower($text);
 header("Content-Type: application/json");
 $response = '';
-
-$token = "941539793:AAGdZEeLppzYtY_J_4tuAf_MyHgjl590DWc";
-
-
-sendMessage($chatid, "[EXECUTED]", $token);
-if(strstr($text, '?')){
-    sendMessage($chatid, "HO TROVATO UN PUNTO INTERROGATIVO", $token);
+if(strpos($text, "/start") === 0 || $text=="ciao")
+{
+	$response = "Ciao $firstname, benvenuto!";
 }
-//elseif($text=="domanda 1")
-//{
-//	$response = "risposta 1";
-//}
-//elseif($text=="domanda 2")
-//{
-//	$response = "risposta 2";
-//}
-//else
-//{
-//	$response = "Comando non valido!";
-//}
-//$parameters = array('chat_id' => $chatId, "text" => $response);
-//$parameters["method"] = "sendMessage";
-//echo json_encode($parameters);
-
-
-
-
-
-
-function sendMessage($chatID, $messaggio, $token) {
-    echo "sending message to " . $chatID . "\n";
-
-    $url = "https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chatID;
-    $url = $url . "&text=" . urlencode($messaggio);
-    $ch = curl_init();
-    $optArray = array(
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true
-    );
-    curl_setopt_array($ch, $optArray);
-    $result = curl_exec($ch);
-    curl_close($ch);
-    return $result;
+elseif($text=="domanda 1")
+{
+	$response = "risposta 1";
 }
+elseif($text=="domanda 2")
+{
+	$response = "risposta 2";
+}
+else
+{
+	$response = "Comando non valido!";
+}
+$parameters = array('chat_id' => $chatId, "text" => $response);
+$parameters["method"] = "sendMessage";
+echo json_encode($parameters);
